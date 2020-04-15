06874f962de77f3c659eb433c5d5ba6f152d5a09
Jose Martinez <jmartinez@Joses-MacBook-Pro-2.local>
2020-04-15 09:57:36 -0400

    Removed dist folder and .tgz since they're not needed

--- dist/esm/web.d.ts
+++
@@ -1,9 +0,0 @@
-import { WebPlugin } from "@capacitor/core";
-import { NativeBiometricPlugin, AvailableOptions } from "./definitions";
-export declare class NativeBiometricWeb extends WebPlugin implements NativeBiometricPlugin {
-    constructor();
-    isAvailable(): Promise<AvailableOptions>;
-    verify(): Promise<any>;
-}
-declare const NativeBiometric: NativeBiometricWeb;
-export { NativeBiometric };
